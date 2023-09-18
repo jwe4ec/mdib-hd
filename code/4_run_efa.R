d3d8600dffeb9e652fe67371a0e5d0d898fa1616
Jeremy Eberle <jwe4ec@virginia.edu>
2023-09-18 07:28:43 -0400

    Run EFA using WLSMV estimator

--- code/4_run_efa.R
+++ code/4_run_efa.R
@@ -36,6 +36,26 @@ groundhog.library(pkgs, groundhog_day)
 set.seed(1234)
 
 # ---------------------------------------------------------------------------- #
+# Define functions used in script ----
+# ---------------------------------------------------------------------------- #
+
+# Define function to export basic results and details to TXT and loadings to CSV
+
+export_res <- function(fit, path, filename_stem) {
+  sink(paste0(path, paste0(filename_stem, ".txt")))
+  print(summary(fit))
+  sink()
+  
+  sink(paste0(path, paste0(filename_stem, "_detail.txt")))
+  print(summary(fit, se = TRUE, zstat = TRUE, pvalue = TRUE))
+  sink()
+  
+  sink(paste0(path, paste0(filename_stem, ".csv")))
+  print(fit$loadings)
+  sink()
+}
+
+# ---------------------------------------------------------------------------- #
 # Import data ----
 # ---------------------------------------------------------------------------- #
 
@@ -158,7 +178,8 @@ eigen(cor(mdib_bl))$values
 # Plot eigenvalues as scree plot to help decide how many factors to retain, which
 # shows an unclear break point between cliff and scree
 
-scree_path <- paste0(efa_path, "scree/")
+all_items_path <- paste0(efa_path,       "all_items/")
+scree_path     <- paste0(all_items_path, "scree/")
 
 dir.create(scree_path, recursive = TRUE)
 
@@ -176,31 +197,42 @@ dev.off()
 # defaults to a "quant" argument of .95, meaning the 95% CI is used as the threshold 
 # rather than the mean, in contrast to the documentation, which needs to be fixed.)
 
-pdf(paste0(scree_path, "mdib_bl_scree_pa.pdf"), height = 6, width = 6)
-(result <- fa.parallel(mdib_bl, fm = "ml", n.iter = 100))
+pdf(paste0(scree_path, "mdib_bl_scree_pa_ml.pdf"), height = 6, width = 6)
+(result_ml <- fa.parallel(mdib_bl, fm = "ml", n.iter = 100))
 dev.off()
 
-result$ncomp == 2
+result_ml$ncomp == 2
 
-sum(result$pc.values > result$pc.sim)  == 4
-sum(result$pc.values > result$pc.simr) == 4
+sum(result_ml$pc.values > result_ml$pc.sim)  == 4
+sum(result_ml$pc.values > result_ml$pc.simr) == 4
 
-# TODO (contact William Revelle about error/warnings, which occur even when the
-# "fm" and "n.iter" arguments are removed, leaving the defaults--also unsure what
-# "fm" method to use in this case given that it seems "WLSMV" is not available): 
 # Also do parallel analysis of polychoric correlations (given some heavily skewed 
-# items--see above)
+# items--see above). Per personal correspondence on 9/17/2023, William Revelle 
+# suggested setting "correct = 0" to avoid error/warnings when setting 'cor = "poly"'.
+# Unclear what estimation method to use for polychoric correlations, so try using
+# both "ML" (as above, but with only 10 iterations because it takes a long time to
+# run) and "minres" (default, including for "fa.parallel.poly()"). Results are same.
 
-pdf(paste0(scree_path, "mdib_bl_scree_pa_poly.pdf"), height = 6, width = 6)
-(result <- fa.parallel(mdib_bl, fm = "ml", n.iter = 100, cor = "poly"))
+pdf(paste0(scree_path, "mdib_bl_scree_pa_poly_ml.pdf"), height = 6, width = 6)
+(result_poly_ml <- fa.parallel(mdib_bl, fm = "ml", n.iter = 10, correct = 0, cor = "poly"))
 dev.off()
 
+result_poly_ml$ncomp == 4
 
+sum(result_poly_ml$pc.values > result_poly_ml$pc.sim)  == 4
+sum(result_poly_ml$pc.values > result_poly_ml$pc.simr) == 3
 
+pdf(paste0(scree_path, "mdib_bl_scree_pa_poly_minres.pdf"), height = 6, width = 6)
+(result_poly_minres <- fa.parallel(mdib_bl, correct = 0, cor = "poly"))
+dev.off()
 
+result_poly_minres$ncomp == 4
+
+sum(result_poly_minres$pc.values > result_poly_minres$pc.sim)  == 4
+sum(result_poly_minres$pc.values > result_poly_minres$pc.simr) == 3
 
 # ---------------------------------------------------------------------------- #
-# Run EFA based on all items ----
+# Run EFA based on all items using "MLM" estimator ----
 # ---------------------------------------------------------------------------- #
 
 # Based on scree plot, considered retaining 2 to 9 factors, but parallel analysis 
@@ -209,8 +241,8 @@ dev.off()
 
 # Note: Random seed must be set for each analysis for reproducible results
 
-# Note: Estimator "MLM" gives two warnings per number of factors about the smallest
-# eigenvalue being negative:
+# Note: For oblimin and geomin, estimator "MLM" gives two warnings per number of 
+# factors about the smallest eigenvalue being negative:
 #   "The variance-covariance matrix of the estimated parameters (vcov) does not 
 #   appear to be positive definite! The smallest eigenvalue (= -8.822709e-16) 
 #   is smaller than zero. This may be a symptom that the model is not identified."
@@ -222,53 +254,20 @@ dev.off()
 # the issue is tied to the robust standard errors and test statistic of "MLM").
 
 set.seed(1234)
-fit_oblimin <- efa(data = mdib_bl, nfactors = 2:5, rotation = "oblimin", estimator = "MLM", check.vcov = FALSE)
+fit_oblimin_mlm <- efa(data = mdib_bl, nfactors = 2:5, rotation = "oblimin", 
+                       estimator = "MLM", check.vcov = FALSE)
 set.seed(1234)
-fit_geomin  <- efa(data = mdib_bl, nfactors = 2:5, rotation = "geomin",  estimator = "MLM", check.vcov = FALSE)
+fit_geomin_mlm  <- efa(data = mdib_bl, nfactors = 2:5, rotation = "geomin",  
+                       estimator = "MLM", check.vcov = FALSE)
 set.seed(1234)
-fit_promax  <- efa(data = mdib_bl, nfactors = 2:5, rotation = "promax",  estimator = "MLM")
+fit_promax_mlm  <- efa(data = mdib_bl, nfactors = 2:5, rotation = "promax",  
+                       estimator = "MLM")
 
-# Export basics to TXT
+# Export basic results and details to TXT and loadings to CSV
 
-sink(paste0(efa_path, "oblimin_mlm.txt"))
-summary(fit_oblimin)
-sink()
-
-sink(paste0(efa_path, "geomin_mlm.txt"))
-summary(fit_geomin)
-sink()
-
-sink(paste0(efa_path, "promax_mlm.txt"))
-summary(fit_promax)
-sink()
-
-# Export details to TXT
-
-sink(paste0(efa_path, "oblimin_mlm_detail.txt"))
-summary(fit_oblimin, se = TRUE, zstat = TRUE, pvalue = TRUE)
-sink()
-
-sink(paste0(efa_path, "geomin_mlm_detail.txt"))
-summary(fit_geomin,  se = TRUE, zstat = TRUE, pvalue = TRUE)
-sink()
-
-sink(paste0(efa_path, "promax_mlm_detail.txt"))
-summary(fit_promax,  se = TRUE, zstat = TRUE, pvalue = TRUE)
-sink()
-
-# Export loadings to CSV
-
-sink(paste0(efa_path, "oblimin_mlm.csv"))
-fit_oblimin$loadings
-sink()
-
-sink(paste0(efa_path, "geomin_mlm.csv"))
-fit_geomin$loadings
-sink()
-
-sink(paste0(efa_path, "promax_mlm.csv"))
-fit_promax$loadings
-sink()
+export_res(fit_oblimin_mlm, all_items_path, "oblimin_mlm")
+export_res(fit_geomin_mlm,  all_items_path, "geomin_mlm")
+export_res(fit_promax_mlm,  all_items_path, "promax_mlm")
 
 # TODO: For reference on EMA, see "twincogFA.R" and "PHysCompFA.R" from 11/4/2019
 # multivariate class with Steve Boker
@@ -278,15 +277,135 @@ sink()
 
 
 # ---------------------------------------------------------------------------- #
-# TODO: Consider running EFA based on all items using WLSMV estimator ----
+# Run EFA based on all items using WLSMV estimator ----
 # ---------------------------------------------------------------------------- #
 
+# Convert columns to ordered factors
+
+mdib_bl_ord <- as.data.frame(lapply(mdib_bl, factor, levels = 0:4, ordered = TRUE))
+
+# For "mdib_ben_int_email_6a", which no participant rated 0, remove the level 0
+# and consider the level 1 to mean "0 or 1." Before doing this, "efa()" yielded
+# the error "some categories of variable mdib_ben_int_email_6a' are empty in group 
+# 1." For items that contain empty response categories, Yves Rosseel recommends 
+# collapsing across two adjacent response categories so categories are no longer
+# empty (see https://groups.google.com/g/lavaan/c/ZvIjcQFTRQY/m/SyY9DOzrsNkJ). He
+# confirmed via personal communication on 9/17/2023 that response categories should
+# be collapsed only for the item with empty response categories (not for all items).
+
+mdib_bl_ord$mdib_ben_int_email_6a <- droplevels(mdib_bl_ord$mdib_ben_int_email_6a)
+
+levels(mdib_bl_ord$mdib_ben_int_email_6a)[levels(mdib_bl_ord$mdib_ben_int_email_6a) == 1] <- "0 or 1"
+
+# Consider 2, 3, 4, or 5 factors, as determined above
+
+# Note: For oblimin and geomin, estimator "WLSMV" gives two warnings per number of 
+# factors about the smallest eigenvalue being negative:
+#   "The variance-covariance matrix of the estimated parameters (vcov) does not 
+#   appear to be positive definite! The smallest eigenvalue (= -7.174704e-15) is 
+#   smaller than zero. This may be a symptom that the model is not identified."
+# However, the smallest eigenvalue across all warnings is -7.591752e-13, which is
+# just barely negative and may simply be due to a machine precision issue (see
+# above). Adding "check.vcov = FALSE" (see above) removes the warnings.
+
+# Note: For oblimin, geomin, and promax and 3-5 factors, "WLSMV" estimator yields
+# the following warning, and inspection of results shows a Heywood case on item 
+# "mdib_neg_int_cough_12b" in which the uniqueness is negative and the commonality 
+# is > 1, which is impossible (see Cooperman & Waller, 2022, https://doi.org/gmdnd7).
+#   "some estimated ov variances are negative"
+# Heywood cases in EFA are more likely in small samples (Cooperman & Waller, 2022).
+# We will interpret only the 2-factor solution, which has no Heywood case. Future
+# research may consider using regularized common factor analysis, which may avoid
+# Heywood cases in small samples (see Study 4 of Cooperman & Waller, 2022, p. 170).
+
+set.seed(1234)
+fit_oblimin_wlsmv <- efa(data = mdib_bl_ord, nfactors = 2:5, rotation = "oblimin", 
+                         estimator = "WLSMV", check.vcov = FALSE)
+set.seed(1234)
+fit_geomin_wlsmv  <- efa(data = mdib_bl_ord, nfactors = 2:5, rotation = "geomin",  
+                         estimator = "WLSMV", check.vcov = FALSE)
+set.seed(1234)
+fit_promax_wlsmv  <- efa(data = mdib_bl_ord, nfactors = 2:5, rotation = "promax",  
+                         estimator = "WLSMV")
+
+# Export basic results and details to TXT and loadings to CSV
+
+export_res(fit_oblimin_wlsmv, all_items_path, "oblimin_wlsmv")
+export_res(fit_geomin_wlsmv,  all_items_path, "geomin_wlsmv")
+export_res(fit_promax_wlsmv,  all_items_path, "promax_wlsmv")
+
+# ---------------------------------------------------------------------------- #
+# Inspect scree plot based on only theorized negative bias items ----
+# ---------------------------------------------------------------------------- #
+
+# Restrict to 12 theorized negative bias items
+
+mdib_bl_neg <- mdib_bl[, names(mdib_bl[grepl("mdib_neg", names(mdib_bl))])]
+
+length(mdib_bl_neg) == 12
+
+# Obtain eigenvalues of correlation matrix
+
+eigen(cor(mdib_bl_neg))$values
+
+# Plot eigenvalues as scree plot to help decide how many factors to retain, which
+# shows an unclear break point between cliff and scree
+
+neg_items_path <- paste0(efa_path,       "neg_items/")
+scree_path     <- paste0(neg_items_path, "scree/")
+
+dir.create(scree_path, recursive = TRUE)
+
+pdf(paste0(scree_path, "mdib_bl_scree.pdf"), height = 6, width = 6)
+scree(mdib_bl_neg, factors = FALSE, pc = TRUE)
+dev.off()
+
+# Given this, also consider parallel analysis, which suggests an upper bound of 1 
+# factor or 1 component (see above)
+
+pdf(paste0(scree_path, "mdib_bl_scree_pa_ml.pdf"), height = 6, width = 6)
+(result_ml <- fa.parallel(mdib_bl_neg, fm = "ml", n.iter = 100))
+dev.off()
+
+result_ml$ncomp == 1
+
+sum(result_ml$pc.values > result_ml$pc.sim) == 1
+sum(result_ml$pc.values > result_ml$pc.simr) == 1
+
+# Also do parallel analysis of polychoric correlations (see above)
+
+pdf(paste0(scree_path, "mdib_bl_scree_pa_poly_ml.pdf"), height = 6, width = 6)
+(result_poly_ml <- fa.parallel(mdib_bl_neg, fm = "ml", n.iter = 10, correct = 0, cor = "poly"))
+dev.off()
+
+result_poly_ml$ncomp == 1
+
+sum(result_poly_ml$pc.values > result_poly_ml$pc.sim)  == 1
+sum(result_poly_ml$pc.values > result_poly_ml$pc.simr) == 1
+
+pdf(paste0(scree_path, "mdib_bl_scree_pa_poly_minres.pdf"), height = 6, width = 6)
+(result_poly_minres <- fa.parallel(mdib_bl_neg, correct = 0, cor = "poly"))
+dev.off()
+
+result_poly_minres$ncomp == 1
+
+sum(result_poly_minres$pc.values > result_poly_minres$pc.sim)  == 1
+sum(result_poly_minres$pc.values > result_poly_minres$pc.simr) == 1
+
+# ---------------------------------------------------------------------------- #
+# Run EFA based on only negative bias items using "MLM" estimator ----
+# ---------------------------------------------------------------------------- #
+
+# TODO: Based on scree plot, considered retaining 1 to 3 factors, but parallel analysis 
+# suggests a smaller number (up to 1, though looking at +/- 1 is recommended; Lim 
+# & Jahng, 2019). Thus, consider 1 or 2.
+
 
 
 
 
 # ---------------------------------------------------------------------------- #
-# TODO: Consider running EFA based on only theorized negative bias items ----
+# TODO: Run EFA based on only negative bias items using "WSLMV" estimator ----
 # ---------------------------------------------------------------------------- #
 
 
